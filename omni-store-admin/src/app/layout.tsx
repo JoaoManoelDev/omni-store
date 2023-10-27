f4ffff5659b761240b5d5012a3c6fc670f3e93f9
João Manoel <joaomanoeldev@gmail.com>
2023-10-27 17:02:42 -0300

    feat: create page sign-in and sign-up and add clerk

--- omni-store-admin/src/app/layout.tsx
+++ omni-store-admin/src/app/layout.tsx
@@ -1,12 +1,14 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import '../styles/globals.css'
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import { ClerkProvider } from "@clerk/nextjs"
+import { ptBR } from "@clerk/localizations"
+import "../styles/globals.css"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: 'Omni Store',
-  description: 'Omni Store',
+  title: "Omni Store",
+  description: "Omni Store",
 }
 
 export default function RootLayout({
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider localization={ptBR}>
+      <html lang="pt-br">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
