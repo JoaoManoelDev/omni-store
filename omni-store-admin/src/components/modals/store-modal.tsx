c50bcc9e0ccd652b3c911f0e3d439281232adb6f
João Manoel <joaomanoeldev@gmail.com>
2023-11-06 17:03:10 -0300

    feat: implementing the onSubmit function to create the store

--- omni-store-admin/src/components/modals/store-modal.tsx
+++ omni-store-admin/src/components/modals/store-modal.tsx
@@ -2,11 +2,13 @@
 
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
+import axios from "axios"
+import toast from "react-hot-toast"
 
 import { Modal } from "@/components/ui/modal"
 import { NewStoreInput, newStoreSchema } from "@/lib/validations/store"
 import { storeModalStore } from "@/stores/store-modal-store"
-import { Input } from "../ui/input"
+import { Input } from "@/components/ui/input"
 import {
   Form,
   FormControl,
@@ -15,14 +17,22 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
-import { Button } from "../ui/button"
+import { Button } from "@/components/ui/button"
 
 export const StoreModal = () => {
   const storeModal = storeModalStore()
 
-  const onSubmit = (data: NewStoreInput) => {
-    console.log(data)
-    // TODO: Create Store
+  const onSubmit = async (data: NewStoreInput) => {
+    try {
+      await axios.post("/api/store", data)
+
+      toast.success("Loja criada.")
+
+      newStoreForm.reset()
+    } catch (error) {
+      console.log("[CREATE STORE MODAL]", error)
+      toast.error("Algo deu errado.")
+    }
   }
 
   const newStoreForm = useForm<NewStoreInput>({
@@ -49,7 +59,7 @@ export const StoreModal = () => {
                 <FormItem>
                   <FormLabel>Nome da loja</FormLabel>
                   <FormControl>
-                    <Input {...field} />
+                    <Input disabled={newStoreForm.formState.isSubmitting} {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -57,8 +67,21 @@ export const StoreModal = () => {
             />
 
             <div className="pt-6 space-x-2 flex items-center justify-end">
-              <Button type="button" variant="outline" onClick={storeModal.onClose}>Cancelar</Button>
-              <Button type="submit">Criar</Button>
+              <Button
+                type="button"
+                variant="outline"
+                onClick={storeModal.onClose}
+                disabled={newStoreForm.formState.isSubmitting}
+              >
+                Cancelar
+              </Button>
+              
+              <Button
+                type="submit"
+                disabled={newStoreForm.formState.isSubmitting}
+              >
+                Criar
+              </Button>
             </div>
           </form>
         </Form>
