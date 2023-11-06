0ba4acfdef3ee60218a5bd5a4f92d513a5b9d6f2
João Manoel <joaomanoeldev@gmail.com>
2023-11-06 16:58:16 -0300

    feat: create api store post

---
+++ omni-store-admin/src/app/api/store/route.ts
@@ -0,0 +1,33 @@
+import { auth } from "@clerk/nextjs"
+import { NextResponse } from "next/server"
+
+import { prismadb } from "@/lib/prismadb"
+
+export async function POST(
+  request: Request,
+) {
+  try {
+    
+    const { userId } = auth()
+
+    if (!userId) return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
+
+    const body = await request.json()
+    const { name } = body
+
+    if (!name) return NextResponse.json({ message: "Name is required" }, { status: 400 })
+
+    const newStore = await prismadb.store.create({
+      data: {
+        name,
+        userId: userId
+      }
+    })
+
+    return NextResponse.json(newStore, { status: 201 })
+
+  } catch (error) {
+    console.log("[CREATE_STORE_ERROR]", error)
+    return NextResponse.json({ message: "Internal error." }, { status: 500 })
+  }
+}
